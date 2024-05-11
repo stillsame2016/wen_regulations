8c502099c36d458280414a75466481ef2051237c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-11 09:56:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -78,6 +78,8 @@ if prompt := st.chat_input("What can I help with?"):
         Based on the provided context, using easy understanding language,  answer the question clear and precise with 
         references and explainations. 
 
+        Please don't mention "provided context".
+
         If no information is provided in the context,  return the result as "Sorry I dont know 
         the answer", don't provide the wrong answer.
 
