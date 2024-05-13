910196bed43cd8e77a1f6584d20e4c042994b8b8
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2024-05-12 17:18:10 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
 import json
-import re
 import requests
 import streamlit as st
-from groq import Groq
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
 
 Groq_KEY = st.secrets["Groq_KEY"]
 Groq_KEY_2 = st.secrets["Groq_KEY_2"]
 
-def role_to_streamlit(role):
-    if role == "model":
-        return "assistant"
-    else:
-        return role
-
-def extract_text_between_question_and_answer(text):
-    # Define the pattern to match "Question:" followed by any text until "Answer"
-    pattern = r'Question:(.*?)Answer'
-    # Use re.DOTALL to make '.' match any character including newlines
-    matches = re.findall(pattern, text, re.DOTALL)
-    return matches
+llm = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY)
+llm2 = ChatGroq(temperature=0, model_name="llama3-70b-8192", api_key=Groq_KEY_2)
 
 st.set_page_config(page_title="NPDES")
 
@@ -35,21 +26,10 @@ for message in st.session_state.chat:
         st.markdown(message['content'])
 st.write("")
 
-# Accept user's next message, add to context, resubmit context to Gemini
-if prompt := st.chat_input("What can I help you with?"):
-    # Display and save the user's input
-    st.chat_message("user").markdown(prompt)
-    st.session_state.chat.append({"role": "user", "content": prompt})
+user_input = PromptTemplate(
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are the expert of National Pollution 
+        Discharge Elimination System (NPDES). 
 
-    response = requests.get(f"https://sparcal.sdsc.edu/api/v1/Utility/regulations?search_terms={prompt}")
-    datasets = json.loads(response.text)
-    datasets = datasets[0:5]
-
-    context = "\n\n===================\n\n".join([dataset["description"] for dataset in datasets])
-
-    request = f"""
-        You are the expert of National Pollution Discharge Elimination System (NPDES). 
-        
         The National Pollutant Discharge Elimination System (NPDES) is a regulatory program implemented by the United 
         States Environmental Protection Agency (EPA) to control water pollution. It was established under the Clean 
         Water Act (CWA) to address the discharge of pollutants into the waters of the United States.
@@ -67,39 +47,43 @@ if prompt := st.chat_input("What can I help you with?"):
         thereby improving and maintaining water quality, protecting aquatic ecosystems, and safeguarding human health. 
         It plays a critical role in preventing water pollution and maintaining the integrity of the nation's water 
         resources.
-    
+
         Based on the provided context, use easy understanding language to answer the question clear and precise with 
-        references and explainations. Please don't mention the term "context" in the answer.
+        references and explanations. Please don't mention the term "context" in the answer.
 
-        If no information is provided in the context,  return the result as "Sorry I dont know 
-        the answer", don't provide the wrong answer.
+        If no information is provided in the context, return the result as "Sorry I dont know 
+        the answer", don't provide the wrong answer or a contradictory answer.
 
-        Context:\n {context}?\n
+        Context:{context}
 
-        Question:\n{prompt}\n
-        Answer:
-    """
+        Question:{question}?
+        
+        Answer: <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+    input_variables=["question", "context"],
+)
 
-    # st.markdown(request)
+rag_chain = user_input | llm | StrOutputParser()
+rag_chain_2 = user_input | llm2 | StrOutputParser()
+
+# Accept user's next message, add to context, resubmit context to Gemini
+if user_input := st.chat_input("What can I help you with?"):
+    # Display and save the user's input
+    st.chat_message("user").markdown(user_input)
+    st.session_state.chat.append({"role": "user", "content": user_input})
+
+    response = requests.get(f"https://sparcal.sdsc.edu/api/v1/Utility/regulations?search_terms={user_input}")
+    datasets = json.loads(response.text)
+    datasets = datasets[0:5]
+    context = "\n".join([dataset["description"] for dataset in datasets])
 
     with st.chat_message("assistant"):
-
-        with st.spinner("We are in the process of retrieving the relevant provisions to give you the best possible answer."):
+        with st.spinner(
+                "We are in the process of retrieving the relevant provisions to give you the best possible answer."):
             try:
-                client = Groq(api_key=Groq_KEY)
-                chat_completion = client.chat.completions.create(
-                    messages=[{"role": "user", "content": request}],
-                    model="llama3-70b-8192")
-                result = chat_completion.choices[0].message.content
-        
-                st.markdown(result)
-                st.session_state.chat.append({"role": "assistant", "content": result})
-            except:
-                client = Groq(api_key=Groq_KEY_2)
-                chat_completion = client.chat.completions.create(
-                    messages=[{"role": "user", "content": request}],
-                    model="llama3-70b-8192")
-                result = chat_completion.choices[0].message.content
-        
-                st.markdown(result)
-                st.session_state.chat.append({"role": "assistant", "content": result})
+                result = rag_chain.invoke({"question": user_input, "context": context})
+            except Exception as e:
+                result = rag_chain_2.invoke({"question": user_input, "context": context})
+
+            st.markdown(result)
+            st.session_state.chat.append({"role": "assistant", "content": result})
+
